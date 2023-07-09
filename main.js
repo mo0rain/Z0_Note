149843be49b9a8448398741f0142769dd3cc469c
mo0rain <962620891@qq.com>
2023-07-09 22:34:23 +0800

    update

--- main.js
+++ main.js
@@ -1,22 +1,72 @@
-const { app, BrowserWindow } = require('electron')
+// 动态加载
+// require('electron-nice-auto-reload')({
+//   rootPath: process.cwd(),
+//   rules: [
+//       {
+//           // run lessc while style.less file is changed
+//           // and this script will change the style.css
+//           // hence reload all windows
+//           action: 'script',
+//           target: 'style\\.less',
+//           // lessc src/css/style.less src/css/style.css
+//           script: 'npm run less'
+//       },
+//       {
+//           // relaunch the app while main process related js files
+//           // were changed
+//           action: 'app.relaunch',
+//           target: 'preload\\.js|main\\.js'
+//       }
+//   ],
+//   ignored: /node_modules/,
+//   log: true,
+//   devToolsReopen: true
+// })
+
+
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 const path = require('path')
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    // x:
+    // y:
+    show: false, //
+    width: 1000, // 宽度
+    height: 800, // 高度
+    icon: "logo.ico",
+    // frame: false,
+
+    // maxHeight: 500,
+    // maxWidth: 500,
+    // minHeight: 500,
+    // minWidth: 500,
+    // resizable: false
+
+
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
+      // __dirname 当前目录
     }
   })
 
+  // 先内容加载
   win.loadFile('index.html')
+  win.on("ready-to-show", () => {
+    win.show()
+  })
+
+
+
 }
 
 app.whenReady().then(() => {
   createWindow()
 
-  app.on('activate', () => {
+  app.on('ready', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
@@ -28,3 +78,9 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+/*
+
+
+
+
+*/
